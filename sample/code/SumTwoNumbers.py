bda23ad83325697718517440f0ac448b3a1acc7d
kshitijarya <kshitijarya@live.com>
2016-04-18 11:38:00 -0400

    Try Except learning for input variables

--- sample/code/SumTwoNumbers.py
+++ sample/code/SumTwoNumbers.py
@@ -8,10 +8,21 @@ def sumProblemString(x, y):
     return 'The sum of {} and {} is {}.'.format(x, y, sum)
 
 def main():
-    print(sumProblemString(2, 3))
-    print(sumProblemString(1234567890123, 535790269358))
-    a = int(input("Enter an integer: "))
-    b = int(input("Enter another integer: "))
-    print(sumProblemString(a, b))
+    
+    print("This is my %d program in %s language..!!" % (1, 'Python'))
+    
+    # print(sumProblemString(2, 3))
+    # print(sumProblemString(1234567890123, 535790269358))
+    
+    try:
+      a = int(input("Enter an integer: "))
+      b = int(input("Enter another integer: "))
+    except ValueError:
+       print("Error: Please enter only integer values.")
+       main()
+    else:
+       print("Identified input values as valid integer values.. Result of their addition is:"+ sumProblemString(a, b))
+        
+    print("Program Completed Successfully, keep running for fun..")
 
 main()
